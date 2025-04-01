33380d179c7fe9ba45c982c03a4aedd6b34d41ab
maningreen <flibbers@protonmail.com>
2025-04-01 17:36:22 -0600

    feat: just missing dependancies
    	modified:   derivation.nix
    	modified:   flake.nix
    	modified:   shell.nix

--- derivation.nix
+++ derivation.nix
@@ -1,19 +1,34 @@
-{ pkgs ? import <nixpkgs> {}, stdenv, ... }: stdenv.mkDerivation {
+{ pkgs ? import <nixpkgs> {}, ... }: pkgs.python3Packages.buildPythonApplication {
   pname = "tux";
   version = "0.1.0";
-
   src = ./.;
 
-  buildInputs = with pkgs; [
-    poetry
-    python313
-    git
+  propagatedBuildInputs = let
+    reactionmenu = pkgs.python3Packages.buildPythonApplication rec {
+      pname = "reactionmenu";
+      version = "3.1.7";
+      src = pkgs.fetchPypi {
+        inherit pname version;
+        hash = "sha256-ENo8GWbeK2Jk/N9yU3NIkjxeFRUBZEN1wl9DC/2HBGM=";
+      };
+      propagatedBuildInputs = [ pkgs.python3Packages.discordpy ];
+    };
+  in with pkgs.python3Packages; [
+    prisma
+    loguru
+    discordpy
+    githubkit
+    httpx
+    python-dotenv
+    sentry-sdk
+    reactionmenu
+    pillow
+    requests
   ];
 
-  buildPhase = ''
-poetry env use 3.13.2
-poetry install
-  '';
-  installPhase = ''
-  '';
+  buildInputs = with pkgs.python3Packages; [
+    poetry-core
+  ];
+
+  format = "pyproject";
 }
